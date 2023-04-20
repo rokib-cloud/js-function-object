2e4c39da53f30b327b78ea90a1455e9a04541822
ROKIB HOSSAIN <rokibh393@gmail.com>
2023-04-21 03:03:53 +0600

    added avg_num

--- problem.js
+++ problem.js
@@ -9,3 +9,15 @@ function bar() {
 
 // var foo = foo();
 // var bar = bar();
+
+
+function make_avg(num1, num2, num3) {
+    var totalNum = num1 + num2 + num3;
+    var avg = totalNum / 3;
+    return avg;
+}
+var num1 = 62;
+var num2 = 72;
+var num3 = 82;
+var avgNum = make_avg(num1, num2, num3);
+// console.log('avg', avgNum)
