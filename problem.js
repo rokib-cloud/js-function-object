6868a4f50084b0f73bcc4057720beed340f7aa3a
ROKIB HOSSAIN <rokibh393@gmail.com>
2023-04-21 03:18:11 +0600

    problem solved

--- problem.js
+++ problem.js
@@ -11,13 +11,28 @@ function bar() {
 // var bar = bar();
 
 
-function make_avg(num1, num2, num3) {
-    var totalNum = num1 + num2 + num3;
-    var avg = totalNum / 3;
-    return avg;
-}
-var num1 = 62;
-var num2 = 72;
-var num3 = 82;
-var avgNum = make_avg(num1, num2, num3);
+// function make_avg(num1, num2, num3) {
+//     var totalNum = num1 + num2 + num3;
+//     var avg = totalNum / 3;
+//     return avg;
+// }
+// var num1 = 62;
+// var num2 = 72;
+// var num3 = 82;
+// var avgNum = make_avg(num1, num2, num3);
 // console.log('avg', avgNum)
+
+function make_avg(arr, size) {
+    let sum = 0;
+    for (i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum / size;
+}
+
+var myArray = [1, 2, 3, 4, 5];
+var arraySize = myArray.length;
+var avg = make_avg(myArray, arraySize);
+console.log(avg)
+
+
